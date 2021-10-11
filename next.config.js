148fb2476a0b75fb835288b6b7c003dc82236aa8
anuar.kulsymanov <shol02rgb@gmail.com>
2021-10-11 17:49:22 +0600

    master

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+
 module.exports = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
